2e4c378dfbb204d88d17d7af10183ba08089ae20
Sanchit Yadav <ysanchit78@gmail.com>
2026-09-19 11:52:01 +0530

    Quick Sort

---
+++ Sorting-Algorithms/Quick_Sort.py
@@ -0,0 +1,14 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+if __name__ == "__main__":
+    arr = [64, 34, 25, -12, 22, 11, 90]
+    print("Original array:", arr)
+    sorted_arr = quick_sort(arr)
+    print("Sorted array:", sorted_arr)
