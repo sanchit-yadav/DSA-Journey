001a87f6b2f959ca99e46e3251db3ff59037b809
Sanchit Yadav <ysanchit78@gmail.com>
2026-09-25 13:54:42 +0530

    Rotate array at k place

--- List & Array Leetcode Problem/A) Easy Level Problems/Right_rotate_Array.py
+++ List & Array Leetcode Problem/A) Easy Level Problems/Right_rotate_Array.py
@@ -1,16 +1,12 @@
-# Right rotate an array by one place
-def Right_rotate(arr):
-    if not arr:
-        return arr  # Return the empty array as is
-    last_element = arr[-1]  # Store the last element
-    for i in range(len(arr) - 1, 0, -1):
-        arr[i] = arr[i - 1]  # Shift elements to the right
-    arr[0] = last_element  # Place the last element at the first position
-    return arr  # Return the rotated array
+# Right rotate an array by K place
+def right_rotate_array(arr, k):
+    n = len(arr)
+    k = k % n  # In case k is greater than n
+    return arr[-k:] + arr[:-k]
 
-# Example usage:
+# Example usage
 if __name__ == "__main__":
-    array = [1, 2, 3, -4, 5, 6, 7, 8, 22, 90, -55]
-    print("Original array:", array)
-    rotated_array = Right_rotate(array)
-    print("Array after right rotation:", rotated_array)
+    arr = [1, 2, 3, 4, 5, 10, -12, 0, 7, -18]
+    k = 25
+    rotated_arr = right_rotate_array(arr, k)
+    print(rotated_arr)
